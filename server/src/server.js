0a44e45c2fb5554c0de7b9994790c0f5ae663e84
Mirshab-dot <mirshab231@gmail.com>
2026-03-04 16:42:53 +0530

    add package.json and package-lock.json; configure dotenv in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import ticketRouter from "./ticketRaising/ticket.routes.js";
@@ -21,7 +23,7 @@ import { connectDB } from "./config/db.js";
 // import searchRoutes from "./routes/vd_searchRoutes.js";
 // import { initializeParcelCronJobs } from "./jobs/ks_ParcelRemaiderJob.js";
 
-dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5001;
