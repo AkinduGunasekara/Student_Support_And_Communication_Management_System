4cfabdf09b57246d98b8490887b379765b336161
JAYASINGHE J M V A <it23845800@my.sliit.lk>
2026-03-24 21:57:36 +0530

    add official messaging frontend pages and message service

---
+++ client/src/officialMesseging/services/messageService.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:5001/api/messages";
+
+export const createMessage = async (data, token) => {
+  return axios.post(API_BASE, data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const getMyMessages = async (token) => {
+  return axios.get(`${API_BASE}/my`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const getLecturerMessages = async (token) => {
+  return axios.get(API_BASE, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const getMessageById = async (id, token) => {
+  return axios.get(`${API_BASE}/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const answerMessage = async (id, answerData, token) => {
+  return axios.patch(`${API_BASE}/${id}/answer`, answerData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const updateVisibility = async (id, visibilityData, token) => {
+  return axios.patch(`${API_BASE}/${id}/visibility`, visibilityData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const markAsNotified = async (id, token) => {
+  return axios.patch(
+    `${API_BASE}/${id}/notified`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
+
+export const deleteMessage = async (id, token) => {
+  return axios.delete(`${API_BASE}/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const getPublicMessages = async (params = {}) => {
+  return axios.get(`${API_BASE}/public`, { params });
+};
